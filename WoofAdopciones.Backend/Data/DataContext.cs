ef712a6e6b0fc138f6ec4898f2b7c96959cd7e8a
Jhonatant98 <73711413+Jhonatant98@users.noreply.github.com>
2023-09-15 11:00:48 -0500

    Update DataContext.cs

--- WoofAdopciones.Backend/Data/DataContext.cs
+++ WoofAdopciones.Backend/Data/DataContext.cs
@@ -4,5 +4,8 @@ namespace WoofAdopciones.Backend.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
     }
 }
