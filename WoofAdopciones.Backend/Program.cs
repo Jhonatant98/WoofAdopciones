520511df253f885a496f8d19fd32e777ec90a425
Jhonatant98 <73711413+Jhonatant98@users.noreply.github.com>
2023-09-15 16:09:48 -0500

    add unit of works

--- WoofAdopciones.Backend/Program.cs
+++ WoofAdopciones.Backend/Program.cs
@@ -1,13 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 using WoofAdopciones.Backend.Data;
 using WoofAdopciones.Backend.Interfaces;
 using WoofAdopciones.Backend.Repositories;
+using WoofAdopciones.Backend.UnitsOfWork;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
@@ -16,7 +16,6 @@ builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositor
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
