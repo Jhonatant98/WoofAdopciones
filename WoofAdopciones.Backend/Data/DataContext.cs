0acb5ac5cffb5b81e8944ba7e6be2ecafecbea9b
unknown <astridchavarria253087@correo.itm.edu.co>
2023-09-16 17:49:04 -0500

    Add CRUD category

--- WoofAdopciones.Backend/Data/DataContext.cs
+++ WoofAdopciones.Backend/Data/DataContext.cs
@@ -12,13 +12,14 @@ namespace WoofAdopciones.Backend.Data
 
         public DbSet<Pet> Pets { get; set; }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
             modelBuilder.Entity<Pet>().HasIndex(c => c.Name).IsUnique();
-
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
         }
     }
 }
