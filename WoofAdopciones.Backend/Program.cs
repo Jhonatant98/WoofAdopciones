4c3fc4eb27e563c48f6e915444782100dda98a13
unknown <astridchavarria253087@correo.itm.edu.co>
2023-09-17 18:06:56 -0500

    Add api generate data with token

--- WoofAdopciones.Backend/Program.cs
+++ WoofAdopciones.Backend/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Sales.Backend.Data;
+using Sales.Backend.Services;
 using System.Text.Json.Serialization;
 using WoofAdopciones.Backend.Data;
 using WoofAdopciones.Backend.Interfaces;
@@ -17,7 +18,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IApiService, ApiService>();
 builder.Services.AddTransient<SeedDb>();
+
 
 var app = builder.Build();
 SeedData(app);
