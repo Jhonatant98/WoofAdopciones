2b16dde1f7854799108a501b97b9dc0856273a9c
JDGP <juangil2574447@correo.itm.edu.co>
2023-09-17 09:05:33 -0500

    add CRUD OrderType

--- WoofAdopciones.Backend/Data/DataContext.cs
+++ WoofAdopciones.Backend/Data/DataContext.cs
@@ -10,11 +10,14 @@ namespace WoofAdopciones.Backend.Data
         }
 
         public DbSet<Pet> Pets { get; set; }
+        public DbSet<OrderType> OrderTypes { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Pet>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<OrderType>().HasIndex(c => c.Name).IsUnique();
         }
     }
 }
