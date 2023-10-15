d87e0796301d489563b74210430130c043327c27
Jhonatant98 <73711413+Jhonatant98@users.noreply.github.com>
2023-10-15 11:47:47 -0500

    update backend and shared

--- WoofAdopciones.Backend/Interfaces/IGenericRepository.cs
+++ WoofAdopciones.Backend/Interfaces/IGenericRepository.cs
@@ -4,13 +4,13 @@ namespace WoofAdopciones.Backend.Interfaces
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<Response<T>> GetAsync(int id);
+        Task<T> GetAsync(int id);
 
-        Task<Response<IEnumerable<T>>> GetAsync();
+        Task<IEnumerable<T>> GetAsync();
 
         Task<Response<T>> AddAsync(T entity);
 
-        Task<Response<T>> DeleteAsync(int id);
+        Task DeleteAsync(int id);
 
         Task<Response<T>> UpdateAsync(T entity);
     }
